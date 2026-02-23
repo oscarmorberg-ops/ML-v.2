6a001492f9e203525cfc63e2128488749b77988e
Oscar <oscarmorberg.gb@outlook.com>
2026-02-23 15:20:01 +0100

    🚀 GitHub Actions CI/CD - CSIO S3 ML Scanner LIVE (4 buckets)

--- s3_ml_scanner.py
+++ s3_ml_scanner.py
@@ -3,8 +3,6 @@ import os
 
 # Använd miljövariabler eller default AWS config
 session = boto3.Session(
-    aws_access_key_id=os.getenv('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
     region_name='eu-north-1'
 )
 
