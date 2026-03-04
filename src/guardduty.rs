c4a6a8541c78e5c6ba8a0eb48aded411c184c1c0
Oscar Morberg <oscarmorberg@example.com>
2026-03-04 19:05:32 +0100

    feat: GuardDuty scan function v1

--- src/guardduty.rs
+++ src/guardduty.rs
@@ -1,2 +1,6 @@
 pub const BUCKET: &str = "oscar-guardduty-findings-eu-north-1";
 
+pub async fn scan() -> Result<Vec<String>, Box<dyn std::error::Error>> {
+    println!("Scanning GuardDuty: {}", BUCKET);
+    Ok(vec!["CRITICAL: Backdoor".to_string()])
+}
