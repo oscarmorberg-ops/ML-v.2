9291467c22d6bf9082e5ca9bed1239299fca1da8
Oscar Morberg <oscarmorberg@example.com>
2026-03-05 12:49:29 +0100

    Deep Hell v2: CNN model stub

---
+++ s3scan_ml.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+model = tf.keras.Sequential([
+    tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(64,64,3)),
+    tf.keras.layers.MaxPooling2D(2,2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(2, activation='softmax')
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy')
